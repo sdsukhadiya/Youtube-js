5d985febacc486f311d11d05429bc0a1a0bcf768
sdsukhadiya <sahilsukhadiya51@gmail.com>
2023-11-01 11:56:59 +0530

    variable

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12995566;
+let accountEmail = "sahil@gmail.com";
+var accountPassword = "22";
+
+accountCity = "Surat";
+accountCity = "vapi";
+
+// const accountId = 135465;
+accountEmail = "sd@gmail.com"
+accountPassword = "22DSF"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);

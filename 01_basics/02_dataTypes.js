a9b3f57f07ac2d4e0409b2005d48c1bd8619bc7f
sdsukhadiya <sahilsukhadiya51@gmail.com>
2023-11-01 12:36:03 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+Datatypes
+/*
+numbe => 23
+String => "";
+Boolean => true/false
+null => sever not ans return null
+undefined => let state;
+*/
+console.log(typeof __);  // check data types
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); object 

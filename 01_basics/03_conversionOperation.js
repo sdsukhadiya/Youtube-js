5b21bec49be921812bb46c97e6caa3c887907ab2
sdsukhadiya <sahilsukhadiya51@gmail.com>
2023-11-01 15:21:13 +0530

    Conversion DataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "45acv";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let checkVelueNumber = Number(score)
+
+//console.log(typeof checkVelueNumber);
+//console.log(checkVelueNumber);
+/*
+"33" => 33
+"33abs" => NaN
+true => 1; fasle => 0
+Number()
+String
+*/
+
+let a = new String("GfG");
+// console.log(a.toString());
+
+let isLoginIda = "Sahil";
+let isLoginId = 1;
+
+let BooleanIsLoginId = Boolean(isLoginId)
+console.log(typeof BooleanIsLoginId);
+console.log(BooleanIsLoginId);
+
+/*
+1 => true 0 => false
+"" => false "dsad" => true
+*/
+
